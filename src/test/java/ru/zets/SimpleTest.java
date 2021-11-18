d2fbd2b17ec35074235b1f63f1f873497c344cbf
artem <poi.ru0130@gmail.com>
2021-11-18 21:27:08 +0300

    first commit

---
+++ src/test/java/ru/zets/SimpleTest.java
@@ -0,0 +1,12 @@
+package ru.zets;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    void simpleTest(){
+        Assertions.assertTrue(true);
+    }
+
+}
